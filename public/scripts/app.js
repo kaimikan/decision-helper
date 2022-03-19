03f9ff24fa0ec3be6e7247091616f43e9b78691a
kaimikan <oihic@protonmail.com>
2022-03-19 15:49:09 +0100

    Variables, Conditions & Functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,88 @@
-// JSX = js xml
 "use strict";
 
-var template = React.createElement("p", { id: "someid" }, "JSX from app.js");
+// JSX = js xml
+
+var app = {
+  title: "Decision Helper",
+  subtitle: "Helping YOU Take Action",
+  options: ["Option 1", "Option 2"]
+};
+
+function getOptions(options) {
+  /*
+  FIXME was trying to do smth like this
+  var optionsListJSX = "";
+  if (options.length > 0)
+    for (var i = 0; i <= options.length; i++)
+      optionsListJSX += <li>{options[i]}</li>;
+  var optionsFullJSX = <ol>{optionsListJSX}</ol>;
+  return { optionsFullJSX };
+  */
+  if (options) return React.createElement(
+    "ol",
+    null,
+    options.map(function (option) {
+      return React.createElement(
+        "li",
+        null,
+        option
+      );
+    })
+  );
+}
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && app.subtitle.length > 0 && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  getOptions(app.options)
+);
+
+// second template with comments
+var user = {
+  name: "Kai",
+  age: 22,
+  location: "Deventer"
+};
+
+function getLocation(location) {
+  if (location) return React.createElement(
+    "p",
+    null,
+    "Location: ",
+    location
+  );
+  // the function will return undefined by default so no need to type out
+  //return undefined;
+}
+
+var templateCredentials = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name /* this is assumed !== undefined*/
+    ? user.name.toUpperCase() + "!" : "Anonymous"
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,64 @@
 // JSX = js xml
 
-var template = <p>Is this JSX from app.js</p>;
+var app = {
+  title: "Decision Helper",
+  subtitle: "Helping YOU Take Action",
+  options: ["Option 1", "Option 2"]
+};
+
+function getOptions(options) {
+  /*
+  FIXME was trying to do smth like this
+  var optionsListJSX = "";
+  if (options.length > 0)
+    for (var i = 0; i <= options.length; i++)
+      optionsListJSX += <li>{options[i]}</li>;
+  var optionsFullJSX = <ol>{optionsListJSX}</ol>;
+  return { optionsFullJSX };
+  */
+  if (options)
+    return (
+      <ol>
+        {options.map((option) => (
+          <li>{option}</li>
+        ))}
+      </ol>
+    );
+}
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && app.subtitle.length > 0 && <p>{app.subtitle}</p>}
+    {getOptions(app.options)}
+  </div>
+);
+
+// second template with comments
+var user = {
+  name: "Kai",
+  age: 22,
+  location: "Deventer"
+};
+
+function getLocation(location) {
+  if (location) return <p>Location: {location}</p>;
+  // the function will return undefined by default so no need to type out
+  //return undefined;
+}
+
+var templateCredentials = (
+  <div>
+    <h1>
+      {user.name /* this is assumed !== undefined*/
+        ? user.name.toUpperCase() + "!"
+        : "Anonymous"}
+    </h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
