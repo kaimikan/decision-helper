0ee65ab143be3af3c5f9e7eadfc6e14e3880b291
kaimikan <oihic@protonmail.com>
2022-03-19 17:38:22 +0100

    Manual Data Binding

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+"use strict";
+
 // JSX = js xml
 
-const app = {
+var app = {
   title: "Decision Helper",
   subtitle: "Helping YOU Take Action",
   options: ["Option 1", "Option 2"]
@@ -11,48 +13,107 @@ function getOptions(options) {
   FIXME Warning: Each child in an array or iterator should have a unique "key" prop.
   */
   if (options)
-    return (
-      <ol>
-        {options.map((option) => (
-          <li>{option}</li>
-        ))}
-      </ol>
+    return React.createElement(
+      "ol",
+      null,
+      options.map(function (option) {
+        return React.createElement("li", null, option);
+      })
     );
 }
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && app.subtitle.length > 0 && <p>{app.subtitle}</p>}
-    {getOptions(app.options)}
-  </div>
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement("h1", null, app.title),
+  app.subtitle &&
+    app.subtitle.length > 0 &&
+    React.createElement("p", null, app.subtitle),
+  getOptions(app.options)
 );
 
-// second template with comments
-const user = {
+// SECOND template with comments
+var user = {
   name: "Kai",
   age: 22,
   location: "Deventer"
 };
 
 function getLocation(location) {
-  if (location) return <p>Location: {location}</p>;
+  if (location) return React.createElement("p", null, "Location: ", location);
   // the function will return undefined by default so no need to type out
   //return undefined;
 }
 
-const templateCredentials = (
-  <div>
-    <h1>
-      {user.name /* this is assumed !== undefined*/
-        ? user.name.toUpperCase() + "!"
-        : "Anonymous"}
-    </h1>
-    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-  </div>
+var templateCredentials = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name /* this is assumed !== undefined*/
+      ? user.name.toUpperCase() + "!"
+      : "Anonymous"
+  ),
+  user.age &&
+    user.age >= 18 &&
+    React.createElement("p", null, "Age: ", user.age),
+  getLocation(user.location)
 );
 
-const appRoot = document.getElementById("app");
+//ReactDOM.render(templateCredentials, appRoot);
 
-ReactDOM.render(template, appRoot);
+// THIRD template
+
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  console.log("add one", count);
+  renderCounterApp();
+};
+var removeOne = function removeOne() {
+  count--;
+  console.log("remove one");
+  renderCounterApp();
+};
+var reset = function reset() {
+  count = 0;
+  console.log("reset");
+  renderCounterApp();
+};
+
+var appRoot = document.getElementById("app");
+
+var renderCounterApp = function renderCounterApp() {
+  // re-rendering is needed if we want the functions to show the updated
+  // value of count, otherwise the variable changes but the JSX remains static
+
+  // behind the scenes react does not re-render everything that remains static
+  // only the changed JSX
+
+  // notice className instead of class
+  var templateCount = React.createElement(
+    "div",
+    null,
+    React.createElement("h1", null, "Count: ", count),
+    React.createElement(
+      "button",
+      { onClick: addOne, id: "add-btn-id", className: "button" },
+      "+1"
+    ),
+    React.createElement(
+      "button",
+      { onClick: removeOne, id: "remove-btn-id", className: "button" },
+      "-1"
+    ),
+    React.createElement(
+      "button",
+      { onClick: reset, id: "reset-btn-id", className: "button" },
+      "reset"
+    )
+  );
+
+  ReactDOM.render(templateCount, appRoot);
+};
+
+renderCounterApp();
