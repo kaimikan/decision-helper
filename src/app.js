667f5e21d8536aab08dfcc9feb2d9de899d3e81e
kaimikan <oihic@protonmail.com>
2022-03-19 20:18:28 +0100

    Forms & Arrays

--- src/app.js
+++ src/app.js
@@ -1,119 +1,58 @@
-"use strict";
-
 // JSX = js xml
 
-var app = {
+const app = {
   title: "Decision Helper",
   subtitle: "Helping YOU Take Action",
-  options: ["Option 1", "Option 2"]
+  options: []
 };
 
 function getOptions(options) {
-  /*
-  FIXME Warning: Each child in an array or iterator should have a unique "key" prop.
-  */
-  if (options)
-    return React.createElement(
-      "ol",
-      null,
-      options.map(function (option) {
-        return React.createElement("li", null, option);
-      })
-    );
+  return (
+    <ol>
+      {options.map((option) => (
+        <li key={option}>{option}</li>
+      ))}
+    </ol>
+  );
 }
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement("h1", null, app.title),
-  app.subtitle &&
-    app.subtitle.length > 0 &&
-    React.createElement("p", null, app.subtitle),
-  getOptions(app.options)
-);
-
-// SECOND template with comments
-var user = {
-  name: "Kai",
-  age: 22,
-  location: "Deventer"
+const onFormSubmit = (e) => {
+  // prevents page refresh
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  // if won't trigger if option is an empty string
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    renderDecisionApp();
+  }
 };
 
-function getLocation(location) {
-  if (location) return React.createElement("p", null, "Location: ", location);
-  // the function will return undefined by default so no need to type out
-  //return undefined;
-}
-
-var templateCredentials = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    user.name /* this is assumed !== undefined*/
-      ? user.name.toUpperCase() + "!"
-      : "Anonymous"
-  ),
-  user.age &&
-    user.age >= 18 &&
-    React.createElement("p", null, "Age: ", user.age),
-  getLocation(user.location)
-);
-
-//ReactDOM.render(templateCredentials, appRoot);
-
-// THIRD template
-
-var count = 0;
-var addOne = function addOne() {
-  count++;
-  console.log("add one", count);
-  renderCounterApp();
-};
-var removeOne = function removeOne() {
-  count--;
-  console.log("remove one");
-  renderCounterApp();
-};
-var reset = function reset() {
-  count = 0;
-  console.log("reset");
-  renderCounterApp();
+const onWipeOptions = () => {
+  app.options = [];
+  renderDecisionApp();
 };
 
-var appRoot = document.getElementById("app");
+const appRoot = document.getElementById("app");
 
-var renderCounterApp = function renderCounterApp() {
-  // re-rendering is needed if we want the functions to show the updated
-  // value of count, otherwise the variable changes but the JSX remains static
+const renderDecisionApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {/* onWipeOptions instead of onWipeOptions() to make a reference instead of a call */}
+      <button onClick={onWipeOptions}>Wipe Options</button>
+      {app.subtitle && app.subtitle.length > 0 && <p>{app.subtitle}</p>}
+      {getOptions(app.options)}
+      {/* or this <ol> {app.options.map((option) => (<li key={option}>{option}</li>))} </ol> */}
 
-  // behind the scenes react does not re-render everything that remains static
-  // only the changed JSX
-
-  // notice className instead of class
-  var templateCount = React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Count: ", count),
-    React.createElement(
-      "button",
-      { onClick: addOne, id: "add-btn-id", className: "button" },
-      "+1"
-    ),
-    React.createElement(
-      "button",
-      { onClick: removeOne, id: "remove-btn-id", className: "button" },
-      "-1"
-    ),
-    React.createElement(
-      "button",
-      { onClick: reset, id: "reset-btn-id", className: "button" },
-      "reset"
-    )
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
   );
 
-  ReactDOM.render(templateCount, appRoot);
+  ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+renderDecisionApp();
