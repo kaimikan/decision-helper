b78ba450966e8655212fd2ee5522a52a512f9cd7
kaimikan <oihic@protonmail.com>
2022-03-20 21:36:35 +0100

    Webpack Switch Base

---
+++ src/playground/pre-webpack-app.js
@@ -0,0 +1,212 @@
+class DecisionHelperApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+
+    this.state = {
+      options: []
+    };
+  }
+
+  // lifecycle methods only available in class React.Component extending elements
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem("options");
+      const options = JSON.parse(json);
+
+      if (options) this.setState(() => ({ options }));
+    } catch (e) {
+      // Do nothing at all
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // we have the if cause the update triggers if options [] is replaced with []
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem("options", json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+
+  // localStorage only works with string data, to save an array we use json (JSON.stringify -> JSON.parse)
+
+  handleDeleteOptions() {
+    // this.setState(() => {
+    //   return {
+    //     options: []
+    //   };
+    // });
+
+    // notice the ({}) brackets when returning an object (otherwise it would be seen as an empty method)
+    this.setState(() => ({ options: [] }));
+  }
+
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }));
+  }
+
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const randomOption = this.state.options[randomNum];
+    alert(randomOption);
+  }
+
+  handleAddOption(option) {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists";
+    }
+
+    // this.setState((prevState) => {
+    //   return {
+    //     // concat creates a third array with the new element instead of changing the state or prevState one
+    //     options: prevState.options.concat(option)
+    //   };
+    // });
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option)
+    }));
+  }
+
+  render() {
+    const title = "Decision Helper";
+    const subtitle = "get some assistance with decision making";
+    return (
+      <div>
+        <Header title={title} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption handleAddOption={this.handleAddOption} />
+      </div>
+    );
+  }
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Default Title"
+};
+
+const Action = (props) => {
+  return (
+    <div>
+      <button disabled={!props.hasOptions} onClick={props.handlePick}>
+        Suggest Action
+      </button>
+    </div>
+  );
+};
+
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Add an option to get started</p>}
+      {props.options.map((option) => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Option = (props) => {
+  return (
+    <div>
+      {props.optionText}
+      <button
+        onClick={(e) => {
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+
+  // e stands for event
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    // this.setState(() => {
+    //   return {
+    //     error // error: error
+    //   };
+    // });
+
+    this.setState(() => ({ error }));
+
+    if (!error) e.target.elements.option.value = "";
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+// // Stateless Functional Component
+// // faster than class based components
+// // used for simple sections which don't require state to manage
+// // good rule of thumb if a class only has a render() method convert it to a stateless func comp
+// const User = (props) => {
+//   return (
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <p>Age: {props.age}</p>
+//     </div>
+//   );
+// };
+// ReactDOM.render(<User name="Kai" age={22} />, document.getElementById("app"));
+
+ReactDOM.render(<DecisionHelperApp />, document.getElementById("app"));
