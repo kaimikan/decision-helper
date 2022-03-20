317e47b8479081b56d4ac643481703ba87e1b11c
kaimikan <oihic@protonmail.com>
2022-03-20 21:59:07 +0100

    Default & Named Imports

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,70 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person_js__ = __webpack_require__(2);
+//import "./utils.js";
+ // { square } is from the export in utils.js
+// default export is outside of brackets or
+// import subtract from "./utils.js"; // since it's default name does not have to match
+
+
 
 console.log("app.js is running!");
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["c" /* square */](5));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](230, 23));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* default */](19, 2));
+
+console.log(__WEBPACK_IMPORTED_MODULE_1__person_js__["c" /* isAdult */](5));
+console.log(__WEBPACK_IMPORTED_MODULE_1__person_js__["a" /* canDrink */](23));
+console.log(__WEBPACK_IMPORTED_MODULE_1__person_js__["b" /* default */](65));
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return subtract; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return square; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return add; });
+console.log("utils.js is running");
+
+const square = (x) => x * x;
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+// export types - 1. default export, 2. named exports
+// # 1 default export
+
+// const subtract = (a, b) => a - b;
+// export default subtract
+// OR
+// export default (a, b) => a - b;
+// # 2 named export
+
+//export const add = (a, b) => a + b;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
+
+const canDrink = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
+
+const isSenior = (age) => age >= 65;
+/* harmony default export */ __webpack_exports__["b"] = (isSenior);
 
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+//import "./utils.js";
+import subtractFromUtils, { square, add } from "./utils.js"; // { square } is from the export in utils.js
+// default export is outside of brackets or
+// import subtract from "./utils.js"; // since it's default name does not have to match
+
+import isSenior, { isAdult, canDrink } from "./person.js";
+
 console.log("app.js is running!");
+console.log(square(5));
+console.log(add(230, 23));
+console.log(subtractFromUtils(19, 2));
+
+console.log(isAdult(5));
+console.log(canDrink(23));
+console.log(isSenior(65));
