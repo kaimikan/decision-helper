5cffa40971d3eafa2735b807002526d21aafb40d
kaimikan <oihic@protonmail.com>
2022-03-21 18:13:29 +0100

    Button Styling

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Action = (props) => (
   <div>
-    <button disabled={!props.hasOptions} onClick={props.handlePick}>
+    <button
+      className="big-button"
+      disabled={!props.hasOptions}
+      onClick={props.handlePick}
+    >
       Suggest Action
     </button>
   </div>
