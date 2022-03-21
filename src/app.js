f17974060a396febdc95e9d28ed4d4e2e20cc261
kaimikan <oihic@protonmail.com>
2022-03-21 17:46:44 +0100

    Normalize CSS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import DecisionHelperApp from "./components/DecisionHelperApp";
+// if you don't specify path it auto looks in node_modules
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 ReactDOM.render(<DecisionHelperApp />, document.getElementById("app"));
