2aa70868170833a8ef77f739862b90d495b66dfd
kaimikan <oihic@protonmail.com>
2022-03-21 17:17:11 +0100

    CSS Connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import DecisionHelperApp from "./components/DecisionHelperApp";
+import "./styles/styles.css";
 
 ReactDOM.render(<DecisionHelperApp />, document.getElementById("app"));
